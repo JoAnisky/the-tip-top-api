19b727845abee2bb67cd8028e901de4ba865c3bc
JoAnisky <aniskydev@protonmail.com>
2025-08-22 15:54:08 +0200

    FEAT : Ticket entity

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TicketRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TicketRepository::class)]
+#[ApiResource]
+class Ticket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTime $issued_on = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $receiptAmount = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIssuedOn(): ?\DateTime
+    {
+        return $this->issued_on;
+    }
+
+    public function setIssuedOn(\DateTime $issued_on): static
+    {
+        $this->issued_on = $issued_on;
+
+        return $this;
+    }
+
+    public function getReceiptAmount(): ?string
+    {
+        return $this->receiptAmount;
+    }
+
+    public function setReceiptAmount(string $receiptAmount): static
+    {
+        $this->receiptAmount = $receiptAmount;
+
+        return $this;
+    }
+}
