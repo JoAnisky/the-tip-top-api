c6713ddaec6641b6f0f2009d0a46721678a03502
JoAnisky <aniskydev@protonmail.com>
2025-08-22 15:58:40 +0200

    FEAT : Code entity

---
+++ src/Entity/Code.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CodeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CodeRepository::class)]
+class Code
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?bool $isUsed = null;
+
+    #[ORM\Column]
+    private ?bool $hasTicket = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $usedOn = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $expiresAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isUsed(): ?bool
+    {
+        return $this->isUsed;
+    }
+
+    public function setIsUsed(bool $isUsed): static
+    {
+        $this->isUsed = $isUsed;
+
+        return $this;
+    }
+
+    public function hasTicket(): ?bool
+    {
+        return $this->hasTicket;
+    }
+
+    public function setHasTicket(bool $hasTicket): static
+    {
+        $this->hasTicket = $hasTicket;
+
+        return $this;
+    }
+
+    public function getUsedOn(): ?\DateTimeImmutable
+    {
+        return $this->usedOn;
+    }
+
+    public function setUsedOn(?\DateTimeImmutable $usedOn): static
+    {
+        $this->usedOn = $usedOn;
+
+        return $this;
+    }
+
+    public function getExpiresAt(): ?\DateTimeImmutable
+    {
+        return $this->expiresAt;
+    }
+
+    public function setExpiresAt(\DateTimeImmutable $expiresAt): static
+    {
+        $this->expiresAt = $expiresAt;
+
+        return $this;
+    }
+}
