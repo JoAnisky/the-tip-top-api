a5e2d143259ed9bd54d6d92476513133616f6d9e
JoAnisky <aniskydev@protonmail.com>
2025-11-12 17:19:59 +0100

    FIX - required dev bundle in production

---
+++ config/packages/dev/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
+];
