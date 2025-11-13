f4ceb7738d7f36ba619f2f5baad57b8c30e8c565
JoAnisky <aniskydev@protonmail.com>
2025-11-13 08:06:47 +0100

    Revert "FIX - required dev bundle in production"
    This reverts commit a5e2d143259ed9bd54d6d92476513133616f6d9e.

--- config/packages/dev/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
-];
