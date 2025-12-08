ef10a839d3d54424c9d0f70ab824b4c9806c3e35
JoAnisky <aniskydev@protonmail.com>
2025-12-08 10:18:48 +0100

    [ADD] SocialAccount : API security + validation constraints

--- src/Entity/SocialAccount.php
+++ src/Entity/SocialAccount.php
@@ -2,23 +2,48 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\SocialAccountRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: SocialAccountRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(security: "is_granted('ROLE_ADMIN')"),
+        new Get(security: "is_granted('ROLE_ADMIN')"),
+        new Post(security: "is_granted('ROLE_USER')"),
+        new Delete(security: "is_granted('ROLE_USER')"),
+    ],
+    normalizationContext: ['groups' => ['socialaccount:read']],
+    denormalizationContext: ['groups' => ['socialaccount:create']],
+)]
 class SocialAccount
 {
+    #[Groups(['socialaccount:read'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 255)]
+    #[Groups(['socialaccount:read', 'socialaccount:create'])]
     #[ORM\Column(length: 255)]
     private ?string $providerName = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Positive]
+    #[Groups(['socialaccount:read', 'socialaccount:create'])]
     #[ORM\Column]
     private ?int $providerId = null;
 
+    #[Groups(['socialaccount:read'])]
     #[ORM\ManyToOne(inversedBy: 'socialAccounts')]
     private ?User $user = null;
 
