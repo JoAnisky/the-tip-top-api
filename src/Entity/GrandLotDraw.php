73c7b9fadbe0bb8a6fa6337a307815c89150a218
JoAnisky <aniskydev@protonmail.com>
2025-12-08 10:05:26 +0100

    [ADD] GrandLotDraw : API security + validation constraints

--- src/Entity/GrandLotDraw.php
+++ src/Entity/GrandLotDraw.php
@@ -2,23 +2,48 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Patch;
 use App\Repository\GrandLotDrawRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: GrandLotDrawRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(security: "is_granted('ROLE_ADMIN')"),
+        new Get(security: "is_granted('ROLE_ADMIN')"),
+        new Post(security: "is_granted('ROLE_ADMIN')"),
+        new Patch(security: "is_granted('ROLE_ADMIN')"),
+    ],
+    normalizationContext: ['groups' => ['grandlotdraw:read']],
+    denormalizationContext: ['groups' => ['grandlotdraw:create', 'grandlotdraw:update']],
+)]
 class GrandLotDraw
 {
+    #[Groups(['grandlotdraw:read'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Groups(['grandlotdraw:read', 'grandlotdraw:create', 'grandlotdraw:update'])]
     #[ORM\Column]
-    private ?\DateTimeImmutable $attribution_date = null;
+    private ?\DateTimeImmutable $attributionDate = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 255)]
+    #[Groups(['grandlotdraw:read', 'grandlotdraw:create', 'grandlotdraw:update'])]
     #[ORM\Column(length: 255)]
     private ?string $label = null;
 
+    #[Assert\NotNull]
+    #[Groups(['grandlotdraw:read'])]
     #[ORM\OneToOne(inversedBy: 'grandLotDraw')]
     #[ORM\JoinColumn(nullable: true)]
     private ?User $winner = null;
@@ -30,12 +55,12 @@ class GrandLotDraw
 
     public function getAttributionDate(): ?\DateTimeImmutable
     {
-        return $this->attribution_date;
+        return $this->attributionDate;
     }
 
-    public function setAttributionDate(\DateTimeImmutable $attribution_date): static
+    public function setAttributionDate(\DateTimeImmutable $attributionDate): static
     {
-        $this->attribution_date = $attribution_date;
+        $this->attributionDate = $attributionDate;
 
         return $this;
     }
