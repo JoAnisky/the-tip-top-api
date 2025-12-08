5fdcd7e40cfc4d4b836bf7f2077478df926dcac3
JoAnisky <aniskydev@protonmail.com>
2025-12-08 15:51:58 +0100

    [FEAT] OwnerAwareInterface : add applyToItem() filter

--- src/Doctrine/OwnerAwareExtension.php
+++ src/Doctrine/OwnerAwareExtension.php
@@ -3,34 +3,52 @@
 namespace App\Doctrine;
 
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
 use App\Interface\OwnerAwareInterface;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Bundle\SecurityBundle\Security;
 
-readonly class OwnerAwareExtension implements QueryCollectionExtensionInterface
+readonly class OwnerAwareExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
     public function __construct(private Security $security) {}
 
     public function applyToCollection(
-        QueryBuilder                $queryBuilder,
+        QueryBuilder $queryBuilder,
         QueryNameGeneratorInterface $queryNameGenerator,
-        string                      $resourceClass,
-        Operation|null              $operation = null,
-        array                       $context = []
+        string $resourceClass,
+        Operation|null $operation = null,
+        array $context = []
     ): void {
+        $this->applyFilter($queryBuilder, $resourceClass);
+    }
 
+    public function applyToItem(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        array $identifiers,
+        Operation|null $operation = null,
+        array $context = []
+    ): void {
+        $this->applyFilter($queryBuilder, $resourceClass);
+    }
+
+    private function applyFilter(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
         if (!is_a($resourceClass, OwnerAwareInterface::class, true)) {
             return;
         }
 
         $user = $this->security->getUser();
 
+        // Les ADMIN voient tout
         if ($this->security->isGranted('ROLE_ADMIN')) {
             return;
         }
 
+        // Les autres utilisateurs ne voient que leurs propres données
         $rootAlias = $queryBuilder->getRootAliases()[0];
         $queryBuilder->andWhere(sprintf('%s.user = :user', $rootAlias))
             ->setParameter('user', $user);
