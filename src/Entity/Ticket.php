02a5cf2fa70d1f7e52f955ceba755a56b07ee616
JoAnisky <aniskydev@protonmail.com>
2025-12-08 10:02:13 +0100

    [ADD] Ticket : API security + validation constraints

--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -3,48 +3,76 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\TicketRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TicketRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new GetCollection(security: "is_granted('ROLE_USER')"),
+        new Get(security: "is_granted('ROLE_USER')"),
+        new Post(security: "is_granted('ROLE_EMPLOYEE')"),
+    ],
+    normalizationContext: ['groups' => ['ticket:read']],
+    denormalizationContext: ['groups' => ['ticket:create']],
+)]
 class Ticket
 {
+    #[Groups(['ticket:read'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Groups(['ticket:read', 'ticket:create'])]
     #[ORM\Column]
-    private ?\DateTime $issued_on = null;
+    private ?\DateTimeImmutable $issuedOn = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Positive]
+    #[Groups(['ticket:read', 'ticket:create'])]
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
     private ?string $receiptAmount = null;
 
+    #[Groups(['ticket:read'])]
     #[ORM\ManyToOne(inversedBy: 'tickets')]
     private ?User $user = null;
 
+    #[Assert\NotNull]
+    #[Groups(['ticket:read', 'ticket:create'])]
     #[ORM\ManyToOne(inversedBy: 'tickets')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Store $store = null;
 
+    #[Groups(['ticket:read'])]
     #[ORM\ManyToOne(inversedBy: 'tickets')]
     private ?Code $code = null;
+
+    public function __construct()
+    {
+        $this->issuedOn = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getIssuedOn(): ?\DateTime
+    public function getIssuedOn(): ?\DateTimeImmutable
     {
-        return $this->issued_on;
+        return $this->issuedOn;
     }
 
-    public function setIssuedOn(\DateTime $issued_on): static
+    public function setIssuedOn(\DateTimeImmutable $issuedOn): static
     {
-        $this->issued_on = $issued_on;
+        $this->issuedOn = $issuedOn;
 
         return $this;
     }
