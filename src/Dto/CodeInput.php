37233bf38403c6056cad084b4285ff8e2aae9b82
JoAnisky <aniskydev@protonmail.com>
2026-01-12 16:25:37 +0100

    [ADD] Dto for Code input

--- src/Dto/CodeInput.php
+++ src/Dto/CodeInput.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace App\Dto;
+
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class CodeInput
+{
+    #[Groups(['code:validate'])]
+    #[Assert\NotBlank(message: "Le code est obligatoire.")]
+    #[Assert\Length(
+        min: 10,
+        max: 10,
+        exactMessage: "Le code doit faire exactement {{ limit }} caractères."
+    )]
+    public string $code;
+}
