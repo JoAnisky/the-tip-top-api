fffa0178fbab532ff184dfc83b23370ed46e4031
Jonathan Loré <aniskydev@protonmail.com>
2026-02-21 17:02:27 +0100

    [ADD] create customers search method

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -16,20 +16,32 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-//    /**
-//     * @return User[] Returns an array of User objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * Cherche un utilisateur (client ROLE_PARTICIPANT uniquement)
+     * @return User[] Returns an array of User objects
+     */
+    public function searchCustomers($value): array
+    {
+        $qb = $this->createQueryBuilder('u');
+
+        return $qb
+            ->where(
+                $qb->expr()->orX(
+                    $qb->expr()->like('LOWER(u.firstName)', ':search'),
+                    $qb->expr()->like('LOWER(u.lastName)', ':search'),
+                    $qb->expr()->like('LOWER(u.email)', ':search'),
+                )
+            )
+            ->andWhere("u.roles NOT LIKE :employee")
+            ->andWhere("u.roles NOT LIKE :admin")
+            ->setParameter('search', '%' . strtolower($value) . '%')
+            ->setParameter('employee', '%ROLE_EMPLOYEE%')
+            ->setParameter('admin', '%ROLE_ADMIN%')
+            ->orderBy('u.lastName', 'ASC')
+            ->setMaxResults(20)
+            ->getQuery()
+            ->getResult();
+    }
 
 //    public function findOneBySomeField($value): ?User
 //    {
